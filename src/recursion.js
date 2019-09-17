3c5494a0c0be1a21f36af55c0baf1e954a9a64e8
alecchampaign <alecchampaign@protonmail.com>
2019-09-17 13:14:03 -0400

    Solve another three problems

--- src/recursion.js
+++ src/recursion.js
@@ -31,17 +31,25 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	if (n < 0) n *= -1;
+	if (n === 0) return true;
+	if (n === 1) return false;
+	return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if (n === 0) return 0;
+	return n > 0 ? (n - 1) + sumBelow(n - 1) : (n + 1) + sumBelow(n + 1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+	if (x === y || x + 1 === y || x - 1 === y) return [];
+	return x < y ? [x + 1].concat(range(x + 1, y)) : [x - 1].concat(range(x - 1, y));
 };
 
 // 7. Compute the exponent of a number.
