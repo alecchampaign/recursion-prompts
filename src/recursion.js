bf8d3e3723a5086f8e55db9e6863995f92cd5ebf
alecchampaign <alecchampaign@protonmail.com>
2019-09-17 13:53:00 -0400

    Solve another three problems

--- src/recursion.js
+++ src/recursion.js
@@ -58,6 +58,9 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+	if (exp === 0) return 1;
+	if (exp === 1 || exp  === -1) return base;
+	return exp > 0 ? base * exponent(base, exp - 1) : 1 / (base * exponent(base, (exp * -1) - 1));
 };
 
 // 8. Determine if a number is a power of two.
@@ -65,10 +68,15 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+	if (n < 1) return false;
+	if (n === 1) return true;
+	return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+	if (string === '') return string;
+	return reverse(string.substr(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
